95258168c7ea1010af4dd655991244185973426f
Slavik191 <slava19970103@gmail.com>
2022-07-27 19:55:35 +0200

    updated

--- admin/directives/table.js
+++ admin/directives/table.js
@@ -2,15 +2,10 @@ import {registerCustomDirective} from '../utils.mjs'
 
 registerCustomDirective('table', {
     fields: [{
-        name: 'table',
-        label: 'Table',
-        widget: "list",
-        label_singular: "row",
-        fields: [{
-          label: "Title", 
-          name: "title", 
-          widget: "string"
-        }]
+      name: 'table',
+      label: 'Table',
+      widget: "list",
+      label_singular: "row"
     }],
     toPreview: ({contents, htmlElement}) => (
         `<span class="sidenote"><span class="sidenote__toggle">${htmlElement}</span><span class="sidenote__note">${contents}</span></span>`
