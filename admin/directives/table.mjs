ca1208b270cfe4526b72ebb9033c39871040b85f
Slavik191 <slava19970103@gmail.com>
2022-07-27 21:19:04 +0200

    updated

--- admin/directives/table.mjs
+++ admin/directives/table.mjs
@@ -4,7 +4,13 @@ registerCustomDirective('table', {
     fields: [{
       name: 'table',
       label: 'Table',
-      widget: "string"
+      widget: "list",
+      label_singular: "row",
+      fields: [{
+        label: "Scorecard Attribute", 
+        name: "scorecard-attribute", 
+        widget: "string"
+      }]
     }],
     toPreview: ({contents, htmlElement}) => (
         `<span class="sidenote"><span class="sidenote__toggle">${htmlElement}</span><span class="sidenote__note">${contents}</span></span>`
