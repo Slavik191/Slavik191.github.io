9a6613d3b53629542eb5097b5befa09aaf6f6e48
Slavik191 <slava19970103@gmail.com>
2022-07-31 17:56:29 +0200

    updated

--- admin/directives/table.mjs
+++ admin/directives/table.mjs
@@ -1,18 +1,66 @@
-import {registerCustomDirective} from '../utils.mjs'
+import { registerCustomDirective } from '../utils.mjs'
 
 registerCustomDirective('table', {
+  fields: [{
+    name: 'rows',
+    label: 'Rows',
+    widget: "list",
+    label_singular: "row",
     fields: [{
-      name: 'table',
-      label: 'Table',
-      widget: "list",
-      label_singular: "row",
-      fields: [{
-        label: "Scorecard Attribute", 
-        name: "scorecard-attribute", 
-        widget: "string"
-      }]
-    }],
-    toPreview: ({contents, htmlElement}) => (
-        `<span class="sidenote"><span class="sidenote__toggle">${htmlElement}</span><span class="sidenote__note">${contents}</span></span>`
-    ),
+      label: "Scorecard Attribute", 
+      name: "scorecard-attribute", 
+      widget: "string"
+    }, {
+      name: "categories",
+      label: "Categories",
+      widget: "select", 
+      options: ["Governance", "Validator market", "Security", "Validator set"]
+  }, {
+    label: "Self-Assessment",
+    name: "self-assessment",
+    widget: "select", 
+    options: ["Good", "Okay", "Needs improvement"]
+  }, {
+    label: "Comments", 
+    name: "comments", 
+    widget: "markdown",
+    buttons: ["link"],
+    editor_components: [],
+    modes: ["rich_text"]
+  }]
+}],
+  toPreview: ({ rows = [] }) => {
+    let rowsString = "";
+    rows.forEach(row => {
+      rowsString = `
+      ${rowsString}
+      ${`
+        <tr>
+          <td>${row["scorecard-attribute"] || ""}</td>
+          <td>${row["categories"] || ""}</td>
+          <td>${row["self-assessment"] || ""}</td>
+          <td>${row["comments"] || ""}</td>
+        </tr>    
+      `}
+    `;
+    });
+
+    return (
+      `
+        <table>
+          <thead>
+            <tr>
+              <th>Scorecard Attribute</th>
+              <th>Category</th>
+              <th>Self-Assessment</th>
+              <th>Comments</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${rowsString}
+          </tbody>
+        </table>
+      `
+    )
+  }
 });
